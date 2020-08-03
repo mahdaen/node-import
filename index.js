df91f1e50d0e95c139cd9a74d272cd9cb62f8d89
Michele Romano <33063403+Mik317@users.noreply.github.com>
2020-08-03 16:18:08 +0200

    Correcting grammar mistake in comment

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ Imports.module = function (source, params, verbose) {
     if ( 'object' == typeof params && !params.length ) {
         for ( var key in params ) {
             if ( params.hasOwnProperty(key) ) {
-                // since `key` is a `variable name`, it's needed verifying the `variable name`
+                // validating the syntax of `key` since it's a variable name
                 var valid_name_regex = "^[a-z_]\\w*$";
                 if(key.match(valid_name_regex) != null)
                     eval('var ' + key + ' = params[key];');
